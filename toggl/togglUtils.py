5b8ce9d53b43d35a9d6fccc7b6aed344c8be666b
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2024-05-24 21:53:13 +0200

    bad gateway...

--- toggl/togglUtils.py
+++ toggl/togglUtils.py
@@ -11,5 +11,9 @@ def handleRequestErrors(response: Response):
         logger.error(f"504: Timeout: {response.text}")
         time.sleep(5)
         return
+    if status_code == 502:
+        logger.error(f"502: Bad Gateway: {response.text}")
+        time.sleep(5)
+        return
     logger.error(f"Request Unexpectedly failed: {response.reason}")
     raise Exception("TogglRequest failed")
