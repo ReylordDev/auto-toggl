29555648fcbb58c57ab552231cfbb4b5637219f0
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2024-05-25 09:29:25 +0200

    increas jr prio

--- windows/specialWindows.py
+++ windows/specialWindows.py
@@ -421,7 +421,7 @@ class Chrome(Window):
 class NvimQT(Window):
     def __init__(self, handle: int):
         super().__init__(handle)
-        self._priority = 4
+        self._priority = 5
 
     def __str__(self):
         return f"Journal(Foreground: {self.is_active()})"
