934974208b6776de80a8ae69f8a1e073b4b62bd7
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2024-05-30 18:54:07 +0200

    fix problem (big problem remains)

--- main.py
+++ main.py
@@ -182,11 +182,18 @@ def main():
                                     )
                     else:
                         logger.info("Current time entry is automatic.")
+                        logger.info("Stopping current time entry.")
                         stop_time_entry(current_time_entry.id)
-                        start_time_entry(
-                            toggl_description=new_description,
-                            toggl_project_id=new_project_id,
-                        )
+                        if new_project_id is None and new_description is None:
+                            logger.info(
+                                "No new project or description. No further action taken."
+                            )
+                        else:
+                            logger.info("Starting new time entry.")
+                            start_time_entry(
+                                toggl_description=new_description,
+                                toggl_project_id=new_project_id,
+                            )
             else:
                 logger.info("No current time entry")
                 if new_project_id is None and new_description is None:
